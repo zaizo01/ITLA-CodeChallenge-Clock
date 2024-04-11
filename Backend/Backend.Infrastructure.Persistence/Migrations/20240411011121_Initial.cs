2a0e78613f8644b151e92b1cda1ef0ee83d8c90c
On3l7d15h <Onelldish@hotmail.com>
2024-04-11 11:49:14 -0400

    CORS Added! ✨🎉

---
+++ Backend/Backend.Infrastructure.Persistence/Migrations/20240411011121_Initial.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChallengeResults",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Level = table.Column<int>(type: "int", maxLength: 10, nullable: false),
+                    CorrectAnswersCount = table.Column<int>(type: "int", nullable: false),
+                    MinutiesElapsed = table.Column<decimal>(type: "decimal(5,3)", precision: 5, scale: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChallengeResults", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Level = table.Column<int>(type: "int", maxLength: 10, nullable: false),
+                    IncorrectAnswser1 = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IncorrectAnswser2 = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IncorrectAnswser3 = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CorrectAnswer = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChallengeResults");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
--- Backend/ITLACodeChallengeClock/Program.cs
+++ Backend/ITLACodeChallengeClock/Program.cs
@@ -47,6 +47,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin()); // Added
 app.UseAuthorization();
 
 app.MapControllers();
